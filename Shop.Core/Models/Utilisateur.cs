95b3e463c21f169bd6cd34095ba7459f408300e4
Woodson Juste <j.woodson@hotmail.fr>
2021-11-18 15:11:57 +0100

    Création de projet :       Couche Shop.Core (Utilisateur, Product, Category,   TypeUtilisateur,   BaseEntity)

---
+++ Shop.Core/Models/Utilisateur.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Models
+{
+    public class Utilisateur : BaseEntity
+    {
+        [Required]
+        [MaxLength(20), MinLength(3)]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(6)]
+        public string ConfirmPWD { get; set; }
+
+        public TypeUtilisateur TypeUtilisateur { get; set; }
+
+        public string Photos { get; set; }
+
+        public Utilisateur()
+        {
+        }
+
+        public Utilisateur(string userName, string email, string password, string confirmPWD)
+        {
+            UserName = userName;
+            Email = email;
+            Password = password;
+            ConfirmPWD = confirmPWD;
+        }
+    }
+}
